f1794aedea4bddbd85dea38c21c75dc20b23cecc
Martin Šimara <r19584@student.osu.cz>
2021-10-14 21:17:55 +0200

    first commit

---
+++ Ukol 2/SIMARA_UKOL_2.ps1
@@ -0,0 +1,9 @@
+﻿$DOWNLOADS = "C:\Users\" + $env:username + "\Downloads"
+$DOCUMENTS = "C:\Users\" + $env:username + "\Documents"
+$PICTURES = "C:\Users\" + $env:username + "\Pictures"
+cd $DOWNLOADS
+Get-ChildItem | Where CreationTime -lt (Get-Date).AddDays(-7) | Remove-Item -Force
+cd $DOCUMENTS
+get-childitem *.jpg, *.png | foreach-object {move-item $_ -destination $PICTURES}
+Get-ChildItem | Where Length -gt 5000000 | Remove-Item -Force
+Write-Host "DONE"
